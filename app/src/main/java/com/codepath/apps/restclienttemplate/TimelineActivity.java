2abe54c8fcfc4c1e171fd514c29bb2e454c94259
Kathy Xing <kathyxing@fb.com>
2022-06-06 16:35:53 -0700

    step 6

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -55,15 +58,31 @@ public class TimelineActivity extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (view == logoutButton) {
-                    onLogoutButton();
-                    finish();
-                    client.clearAccessToken();
-                }
+                onLogoutButton();
             }
         });
 
         populateHomeTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            // Compose icon has been selected
+            Toast.makeText(this,  "Compose!", Toast.LENGTH_SHORT).show();
+
+            // Navigate to the compose activity
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -89,6 +108,8 @@ public class TimelineActivity extends AppCompatActivity {
     }
 
     void onLogoutButton() {
+        finish();
+
         // forget who's logged in
         TwitterApp.getRestClient(this).clearAccessToken();
 
